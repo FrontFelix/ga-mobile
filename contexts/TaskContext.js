664df143eb1dad5d6700dd27ed1f4e08dd0d54e1
Emil <emil.hagelin@medieinstitutet.se>
2023-04-27 13:11:22 +0200

    added some logic for get products (not done)

--- contexts/TaskContext.js
+++ contexts/TaskContext.js
@@ -1,16 +1,18 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import * as Location from "expo-location";
 import { haverSine } from "../hooks/mathHooks";
-import { getContainers } from "../hooks/scannerHooks";
+import { getContainers, getProducts } from "../hooks/scannerHooks";
 export const TaskContext = createContext({
   markContainerAsEmpty: (containerID) => undefined,
   confirmRouteWithContainers: async (containers) => undefined,
   confirmCompletedJob: () => undefined,
+  inventoryProductsList: () => undefined,
   onContainerSelected: async (container, setSpinner) => undefined,
   containers: [],
   routeContainers: [],
   hasActiveJob: false,
   hasCompletedJob: false,
+  products: [],
 });
 
 export const TaskProvider = ({ children }) => {
@@ -18,6 +20,7 @@ export const TaskProvider = ({ children }) => {
   const [hasActiveJob, setActiveJob] = useState(false);
   const [hasCompletedJob, setCompletedJob] = useState(false);
   const [routeContainers, setRouteContainers] = useState([]);
+  const [products, setProducts] = useState([]);
 
   const confirmCompletedJob = () => {
     const updatedContainers = containers.map((item) => {
@@ -30,197 +33,215 @@ export const TaskProvider = ({ children }) => {
       };
       return updatedItem;
     });
-    setContainers(updatedContainers);
-    setActiveJob(false);
-    setCompletedJob(false);
-    setRouteContainers([]);
-    console.log("route längd", routeContainers.length);
-    console.log("route array", routeContainers);
-  };
 
-  const markContainerAsEmpty = (containerID) => {
-    const existingContainer = routeContainers.find(
-      (item) => item.id === containerID
-    );
-    if (existingContainer && !existingContainer.empty) {
-      const updatedArray = routeContainers.map((container) => {
-        if (container.id === containerID) {
-          return { ...container, empty: true };
-        } else {
+    const inventoryProductsList = () => {
+      const productList = products.map((item) => {});
+
+      setContainers(updatedContainers);
+      setActiveJob(false);
+      setCompletedJob(false);
+      setRouteContainers([]);
+      setProducts(productList);
+      console.log("route längd", routeContainers.length);
+      console.log("route array", routeContainers);
+    };
+
+    const markContainerAsEmpty = (containerID) => {
+      const existingContainer = routeContainers.find(
+        (item) => item.id === containerID
+      );
+      if (existingContainer && !existingContainer.empty) {
+        const updatedArray = routeContainers.map((container) => {
+          if (container.id === containerID) {
+            return { ...container, empty: true };
+          } else {
+            return container;
+          }
+        });
+        setRouteContainers(updatedArray);
+      } else {
+        if (existingContainer.empty) {
+          console.log("Container redan tömd...");
+        } else if (!existingContainer) {
+          console.log("finns ingen container");
+        }
+      }
+    };
+
+    const confirmRouteWithContainers = async () => {
+      const confirmedContainers = routeContainers.map((container) => {
+        return { ...container, empty: false };
+      });
+      setRouteContainers(confirmedContainers);
+      setActiveJob(true); // Sätter att man har ett job aktivt.
+
+      // Måste uppdatera varje container till Neptune
+    };
+
+    const onContainerSelected = async (container, setSpinner) => {
+      if (setSpinner) {
+        console.log("spinner aktiv...");
+        setSpinner(true);
+      }
+
+      console.log("ändrar container", container);
+
+      let updatedList = containers.map((item) => {
+        if (item.id === container.id) {
           return container;
+        } else {
+          return item;
         }
       });
-      setRouteContainers(updatedArray);
-    } else {
-      if (existingContainer.empty) {
-        console.log("Container redan tömd...");
-      } else if (!existingContainer) {
-        console.log("finns ingen container");
-      }
-    }
-  };
-
-  const confirmRouteWithContainers = async () => {
-    const confirmedContainers = routeContainers.map((container) => {
-      return { ...container, empty: false };
-    });
-    setRouteContainers(confirmedContainers);
-    setActiveJob(true); // Sätter att man har ett job aktivt.
-
-    // Måste uppdatera varje container till Neptune
-  };
-
-  const onContainerSelected = async (container, setSpinner) => {
-    if (setSpinner) {
-      console.log("spinner aktiv...");
-      setSpinner(true);
-    }
-
-    console.log("ändrar container", container);
-
-    let updatedList = containers.map((item) => {
-      if (item.id === container.id) {
-        return container;
-      } else {
-        return item;
-      }
-    });
-    //console.log("listan från selectedFunction", updatedList);
-    let mapList = [];
-    let noMapList = [];
-    for (item of updatedList) {
-      if (item.routeSelected) {
-        mapList.push(item);
-      } else {
-        noMapList.push(item);
-      }
-    }
-    let updatedMapList = await generateRouteWithContainers(mapList);
-    for (item of noMapList) {
-      const updatedItem = {
-        ...item,
-        marker: [
-          { latitude: 0, longitude: 0 },
-          { latitude: 0, longitude: 0 },
-        ],
-      };
-      updatedMapList.push(updatedItem);
-    }
-
-    console.log("map containers", updatedMapList);
-    setContainers(updatedMapList);
-    const selectedContainers = updatedMapList.filter(
-      (container) => container.routeSelected === true
-    );
-    setRouteContainers(selectedContainers);
-    console.log("selectade containers", selectedContainers);
-    console.log("spinner inte aktiv");
-    if (setSpinner) {
-      setSpinner(false);
-    }
-  };
-  const generateRouteWithContainers = async (containers) => {
-    const markersPosition = [];
-    const { coords } = await Location.getCurrentPositionAsync({});
-
-    let previousContainer = null; // Håller reda på föregående container
-
-    while (containers.length > 0) {
-      let closestContainer = null;
-      let closestDistance = null;
-
-      // Loopa igenom containrarna och jämför avstånden för att hitta närmaste
-      for (const container of containers) {
-        const distance = await haverSine(
-          previousContainer ? previousContainer.location.lat : coords.latitude,
-          previousContainer
-            ? previousContainer.location.long
-            : coords.longitude,
-          container.location.lat,
-          container.location.long
-        );
-
-        if (closestContainer === null || distance < closestDistance) {
-          closestContainer = container;
-          closestDistance = distance;
+      //console.log("listan från selectedFunction", updatedList);
+      let mapList = [];
+      let noMapList = [];
+      for (item of updatedList) {
+        if (item.routeSelected) {
+          mapList.push(item);
+        } else {
+          noMapList.push(item);
         }
       }
+      let updatedMapList = await generateRouteWithContainers(mapList);
+      for (item of noMapList) {
+        const updatedItem = {
+          ...item,
+          marker: [
+            { latitude: 0, longitude: 0 },
+            { latitude: 0, longitude: 0 },
+          ],
+        };
+        updatedMapList.push(updatedItem);
+      }
 
-      // Lägg till den närmaste containern i listan av markörpositioner
-      markersPosition.push({
-        ...closestContainer,
-        marker: [
-          {
-            latitude: previousContainer
+      console.log("map containers", updatedMapList);
+      setContainers(updatedMapList);
+      const selectedContainers = updatedMapList.filter(
+        (container) => container.routeSelected === true
+      );
+      setRouteContainers(selectedContainers);
+      console.log("selectade containers", selectedContainers);
+      console.log("spinner inte aktiv");
+      if (setSpinner) {
+        setSpinner(false);
+      }
+    };
+    const generateRouteWithContainers = async (containers) => {
+      const markersPosition = [];
+      const { coords } = await Location.getCurrentPositionAsync({});
+
+      let previousContainer = null; // Håller reda på föregående container
+
+      while (containers.length > 0) {
+        let closestContainer = null;
+        let closestDistance = null;
+
+        // Loopa igenom containrarna och jämför avstånden för att hitta närmaste
+        for (const container of containers) {
+          const distance = await haverSine(
+            previousContainer
               ? previousContainer.location.lat
               : coords.latitude,
-            longitude: previousContainer
+            previousContainer
               ? previousContainer.location.long
               : coords.longitude,
-          },
-          {
-            latitude: closestContainer.location.lat,
-            longitude: closestContainer.location.long,
-          },
-        ],
+            container.location.lat,
+            container.location.long
+          );
+
+          if (closestContainer === null || distance < closestDistance) {
+            closestContainer = container;
+            closestDistance = distance;
+          }
+        }
+
+        // Lägg till den närmaste containern i listan av markörpositioner
+        markersPosition.push({
+          ...closestContainer,
+          marker: [
+            {
+              latitude: previousContainer
+                ? previousContainer.location.lat
+                : coords.latitude,
+              longitude: previousContainer
+                ? previousContainer.location.long
+                : coords.longitude,
+            },
+            {
+              latitude: closestContainer.location.lat,
+              longitude: closestContainer.location.long,
+            },
+          ],
+        });
+
+        // Uppdatera previousContainer och ta bort den valda containern från listan med containrar
+        previousContainer = closestContainer;
+        containers = containers.filter(
+          (container) => container !== closestContainer
+        );
+      }
+
+      return markersPosition;
+    };
+
+    const loadContainers = async () => {
+      const containersFetch = await getContainers();
+      const updatedContainers = containersFetch.map((container) => {
+        return {
+          ...container,
+          marker: [
+            { latitude: 0, longitude: 0 },
+            { latitude: 0, longitude: 0 },
+          ],
+          routeSelected: false,
+        };
       });
+      setContainers(updatedContainers);
+    };
 
-      // Uppdatera previousContainer och ta bort den valda containern från listan med containrar
-      previousContainer = closestContainer;
-      containers = containers.filter(
-        (container) => container !== closestContainer
-      );
-    }
+    const loadProducts = async () => {
+      const productsFetch = await getProducts();
+      const updatedProducts = productsFetch.map((products) => {});
+      setProducts(updatedProducts);
+    };
 
-    return markersPosition;
+    // USEEFFECTS
+    useEffect(() => {
+      loadContainers();
+    }, []);
+
+    useEffect(() => {
+      if (
+        routeContainers.length &&
+        routeContainers.every((item) => item.empty === true)
+      ) {
+        setCompletedJob(true);
+        console.log("alla är tömda", routeContainers);
+      }
+    }, [routeContainers]);
+
+    useEffect(() => {
+      loadProducts();
+    }, []);
+
+    return (
+      <TaskContext.Provider
+        value={{
+          confirmRouteWithContainers,
+          containers,
+          hasActiveJob,
+          markContainerAsEmpty,
+          onContainerSelected,
+          routeContainers,
+          hasCompletedJob,
+          confirmCompletedJob,
+        }}
+      >
+        {children}
+      </TaskContext.Provider>
+    );
   };
-
-  const loadContainers = async () => {
-    const containersFetch = await getContainers();
-    const updatedContainers = containersFetch.map((container) => {
-      return {
-        ...container,
-        marker: [
-          { latitude: 0, longitude: 0 },
-          { latitude: 0, longitude: 0 },
-        ],
-        routeSelected: false,
-      };
-    });
-    setContainers(updatedContainers);
-  };
-
-  // USEEFFECTS
-  useEffect(() => {
-    loadContainers();
-  }, []);
-
-  useEffect(() => {
-    if (
-      routeContainers.length &&
-      routeContainers.every((item) => item.empty === true)
-    ) {
-      setCompletedJob(true);
-      console.log("alla är tömda", routeContainers);
-    }
-  }, [routeContainers]);
-
-  return (
-    <TaskContext.Provider
-      value={{
-        confirmRouteWithContainers,
-        containers,
-        hasActiveJob,
-        markContainerAsEmpty,
-        onContainerSelected,
-        routeContainers,
-        hasCompletedJob,
-        confirmCompletedJob,
-      }}
-    >
-      {children}
-    </TaskContext.Provider>
-  );
 };
 
 export const useTaskContext = () => useContext(TaskContext);
